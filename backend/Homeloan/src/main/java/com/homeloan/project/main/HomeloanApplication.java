8ee9848d38f0aa359fa7bedaf5274219233f2caa
Sahil Mhatre <sahilmhatrre001@github.com>
2021-09-21 12:30:27 +0530

    Added Skeleton for spring backend

---
+++ backend/Homeloan/src/main/java/com/homeloan/project/main/HomeloanApplication.java
@@ -0,0 +1,13 @@
+package com.homeloan.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeloanApplication.class, args);
+	}
+
+}
